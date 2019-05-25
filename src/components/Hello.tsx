9116a9db8e27ca7f7d2195702354097e7e828e2a
qianjiacheng <jiacheng.qian@manaowan.com>
2019-05-25 18:33:45 +0800

    use less

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import * as style from './hello.less';
+
 export interface HelloProps {
   compiler: string;
   framework: string;
@@ -8,7 +10,7 @@ export interface HelloProps {
 export const Hello = (props: HelloProps): React.FunctionComponentElement<HTMLElement> => {
   const { compiler, framework } = props;
   return (
-    <h1>
+    <h1 className={style.title}>
       Hello from {compiler} and {framework}!
     </h1>
   );
