5c70ee7879d8fe08b4ef7ea6f7422d9cbf6ad5fc
Tan90qian <1436811949@qq.com>
2020-02-17 13:22:27 +0800

    fix

--- src/declares/Component.ts
+++ src/declares/Component.ts
@@ -1,12 +1,12 @@
 import { ComponentType } from 'react';
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps as ReactRouteComponentProps } from 'react-router';
 
 import { IRouterData } from 'src/router/router';
 import { UserStore } from 'src/stores/UserStore';
 import { GlobalStore } from 'src/stores/GlobalStore';
 import { UserService } from 'src/services/user';
 
-export interface RouteComponentProps extends RouteComponentProps {
+export interface RouteComponentProps extends ReactRouteComponentProps {
   routerData: IRouterData;
 }
 
